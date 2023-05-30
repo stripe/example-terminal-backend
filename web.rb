9d7ee39a9c75634eed54e84b1250c0ee3372033a
mus777 <31192698+mus777@users.noreply.github.com>
2023-05-29 21:03:47 -0500

    Update web.rb

--- web.rb
+++ web.rb
@@ -43,9 +43,7 @@ def validateApiKey
   if Stripe.api_key.start_with?('pk')
     return "Error: you used a publishable key to set up the example backend. Please use your test mode secret key. For more information, see https://stripe.com/docs/keys"
   end
-  if Stripe.api_key.start_with?('sk_live')
-    return "Error: you used a live mode secret key to set up the example backend. Please use your test mode secret key. For more information, see https://stripe.com/docs/keys#test-live-modes"
-  end
+ 
   return nil
 end
 
